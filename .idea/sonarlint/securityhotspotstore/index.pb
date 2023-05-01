769d8f12d63176426ebd1f7fb9029a1c6ef8ead6
arjun <arjunbach@gmail.com>
2023-05-01 19:14:29 +0530

    pushing repo

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+[
++src/main/java/org/example/PasteBinPage.java,0\2\028d7e73647a7b0a617b8c1cc7e55c33788b4027
+V
+&src/test/java/org/example/AppTest.java,4\3\438c56474209176629917f1aaaac4e1cf31f765e
---
+++ src/main/java/org/example/PasteBinPage.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class PasteBinPage{
+    WebDriver driver;
+    By code = By.id("postform-text");
+    By paste_expire = By.id("select2-postform-expiration-container");
+
+    By expire = By.xpath("//span[@class= 'select2-results']/ul/li[3]");
+
+    By title = By.id("postform-name");
+
+    By button = By.xpath("//button[text() = 'Create New Paste']");
+
+    public PasteBinPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+    public void NewPaste(){
+        driver.findElement(code).sendKeys("Hello from WebDriver");
+    }
+
+    public void paste_Expiration()  {
+        driver.findElement(paste_expire).click();
+    }
+    public void expireTime(){
+        driver.findElement(expire).click();
+    }
+    public void pasteName(){
+        driver.findElement(title).sendKeys("helloweb");
+    }
+    public void Submit(){
+        driver.findElement(button).click();
+    }
+
+}
